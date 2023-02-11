f26a117c3b3a7dd691231d58096f0a78a824ba84
benszabo <52047541+benszabo@users.noreply.github.com>
2023-02-11 13:56:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import pdfFile from './Tokyo_Menu.pdf';  // replace 'sample.pdf' with your PDF file name
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <iframe
+          src={pdfFile}
+          title="PDF Viewer"
+          className="pdf-viewer"
+        />
     </div>
   );
 }
