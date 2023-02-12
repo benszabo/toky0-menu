113546319cd17ca07b8db69dbd0a47de8fed142c
benszabo <52047541+benszabo@users.noreply.github.com>
2023-02-12 09:19:09 -0500

    im stupid af

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React, { Component } from 'react';
 import './App.css';
-import PDFViewer from './components/PDFViewer';
-import PDFJSBackend from './backends/pdfjs';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <PDFViewer 
-          backend={PDFJSBackend}
-          src='/Tokyo_Menu.pdf'
-        />      
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
 }
 
 export default App;
